b8696f2f03055907028796b54769820cc2172c26
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-20 17:40:25 +0530

    added routes

--- expanse-fe/src/Components/context/globalContext.jsx
+++ expanse-fe/src/Components/context/globalContext.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "https://spendwise-rko5.onrender.com";
+const BASE_URL = "https://spendwise-rko5.onrender.com/api/v1";
 
 
 const GlobalContext = React.createContext()
