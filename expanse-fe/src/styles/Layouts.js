aacefe067b1fe85cac3d00281f65a7368e3ea87c
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-20 17:13:04 +0530

    frontend done

---
+++ expanse-fe/src/styles/Layouts.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainLayout = styled.div`
+  padding: 2rem;
+  height: 100%;
+  display: flex;
+  gap: 2rem;
+`;
+
+export const InnerLayout = styled.div`
+  padding: 1.5rem 3rem;
+  width: 100%;
+`;
