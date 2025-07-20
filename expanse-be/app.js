082a85e47862e2477cd9806e378f869577daf1b0
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-20 17:49:52 +0530

    error

--- expanse-be/app.js
+++ expanse-be/app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://spendwise-1-jd2s.onrender.com',
     credentials: true
 }));
 
