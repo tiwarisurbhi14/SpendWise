c0eb3ee559b1d5b26267818bfae1bb7a0dd3e16a
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-23 00:51:46 +0530

    changesss

--- expanse-be/routes/authRoutes.js
+++ expanse-be/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, logout, getMe } = require('../controllers/authController');
+const { register, login, logout, getMe } = require('../controllers/authControllers');
 const { requireAuth } = require('../middleware/authMiddleware');
 
 router.post('/register', register);
