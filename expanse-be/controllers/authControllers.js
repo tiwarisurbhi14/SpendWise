408738bcf96cf43c26d68a0de2bc40f2c00ddf44
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-23 01:26:27 +0530

    commit

--- expanse-be/controllers/authControllers.js
+++ expanse-be/controllers/authControllers.js
@@ -10,6 +10,10 @@ const createToken = (userId) => {
 
 exports.register = async (req, res) => {
   const { name, email, password } = req.body;
+  console.log(req.body);
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
   try {
     if (await User.findOne({ email })) {
       return res.status(400).json({ error: 'Email already in use' });
