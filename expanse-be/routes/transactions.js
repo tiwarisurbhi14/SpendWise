9e0b69658812076e90715a83ccbb4c5be4ac23d1
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-25 18:42:37 +0530

    added requireauth

--- expanse-be/routes/transactions.js
+++ expanse-be/routes/transactions.js
@@ -1,15 +1,16 @@
 const { addIncome,getIncomes,deleteIncome } = require("../controllers/income");
 const {addExpense,getExpenses,deleteExpense} = require("../controllers/expense");
+import { requireAuth } from "../middleware/authMiddleware";
 
 const router = require("express").Router();
 
 router
-  .post("/add-income", addIncome)
-  .get("/get-incomes", getIncomes)
-  .delete("/delete-income/:id", deleteIncome)
-  .post("/add-expense", addExpense)
-  .get("/get-expenses", getExpenses)
-  .delete("/delete-expense/:id", deleteExpense);
+  .post("/add-income", requireAuth,addIncome)
+  .get("/get-incomes",requireAuth ,getIncomes)
+  .delete("/delete-income/:id", requireAuth, deleteIncome)
+  .post("/add-expense", requireAuth, addExpense)
+  .get("/get-expenses", requireAuth, getExpenses)
+  .delete("/delete-expense/:id", requireAuth, deleteExpense);
 
 
 module.exports = router;
